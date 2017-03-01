157a41f1be591c23b6e921786df98b80c775f315
Dennis Sousa <Dennis Sousa>
2017-03-01 19:02:12 -0300

    agencia

--- features/step_definitions/agencias_santander.rb
+++ features/step_definitions/agencias_santander.rb
@@ -7,16 +7,12 @@ end
 
 Quando(/^acessar a area de agências$/) do
   click_link("Clique aqui")
-  sleep 5
-  new_window = window_opened_by {click_link'https://www.santander.com.br/br/busca-de-agencia'}
-	within_window new_window do
+  page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
   assert_text("Encontre a agência mais próxima de você.")
-  	page.within_window(login_window) do
-  		end
-  	end
-
 end
 
 Então(/^devo selecionar a agência mais proxima$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+fill_in("refCep", :with => "06414025")
+find("#OpcaoBuscaAgencia").click
+
 end
