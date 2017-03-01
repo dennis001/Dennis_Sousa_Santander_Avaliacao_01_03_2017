102331a14e8813966499b281fcea0ba78e3713fd
Dennis Sousa <Dennis Sousa>
2017-03-01 16:36:43 -0300

    agencias

--- features/step_definitions/agencias_santander.rb
+++ features/step_definitions/agencias_santander.rb
@@ -2,10 +2,19 @@
 Dado(/^que esteja na home do site$/) do
   visit "https://www.santander.com.br"
   assert_text("Agências")
+  click_link("Agências")
 end
 
 Quando(/^acessar a area de agências$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link("Clique aqui")
+  sleep 5
+  new_window = window_opened_by {click_link'https://www.santander.com.br/br/busca-de-agencia'}
+	within_window new_window do
+  assert_text("Encontre a agência mais próxima de você.")
+  	page.within_window(login_window) do
+  		end
+  	end
+
 end
 
 Então(/^devo selecionar a agência mais proxima$/) do
