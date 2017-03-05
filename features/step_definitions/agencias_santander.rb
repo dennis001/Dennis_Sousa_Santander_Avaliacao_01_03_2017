8a3c347a8fbb49bffab341f55ac8733da9a3e9ab
Dennis Sousa <Dennis Sousa>
2017-03-05 11:23:15 -0300

    buscar agencias

--- features/step_definitions/agencias_santander.rb
+++ features/step_definitions/agencias_santander.rb
@@ -7,13 +7,14 @@ end
 
 Quando(/^acessar a area de agências$/) do
   click_link("Clique aqui")
-  page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
-  assert_text("Encontre a agência mais próxima de você.")
+    
 end
 
 Então(/^devo selecionar a agência mais proxima$/) do
-assert_text("Cidade*")
-#fill_in("refCep", :with => "06414025")
-#find("#OpcaoBuscaAgencia").click
-
+page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
+within_frame(find("iframe")) do
+fill_in "refCep", :with => "04534011"
+sleep 5
+ find(:css,'#BuscaAgenProximaForm > ul.botoes > li.alignR > a > img').click
+	end
 end
