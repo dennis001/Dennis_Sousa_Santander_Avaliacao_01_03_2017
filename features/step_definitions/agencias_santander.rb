13f3eed0447d7d4eb8cd5da7ee3d4bb5eaa75b63
Dennis Sousa <Dennis Sousa>
2017-03-13 17:59:36 -0300

    buscar agencias

--- features/step_definitions/agencias_santander.rb
+++ features/step_definitions/agencias_santander.rb
@@ -7,14 +7,15 @@ end
 
 Quando(/^acessar a area de agências$/) do
   click_link("Clique aqui")
-    
 end
 
 Então(/^devo selecionar a agência mais proxima$/) do
 page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
-within_frame(find("iframe")) do
-fill_in "refCep", :with => "04534011"
- find(:css,'#BuscaAgenProximaForm > ul.botoes > li.alignR > a > img').click
- sleep 5
-	
-end
+	within_frame(find("iframe")) do
+		fill_in "refCep", :with => "04534011"
+ 		find(:css,'#BuscaAgenProximaForm > ul.botoes > li.alignR > a > img').click
+ 	select "R Joaquim Floriano ,27 - São Paulo", :from => "refEndereco"
+		assert_text("Agência(s) encontrada (s): 100 agência(s)")
+	sleep 5
+	end
+ end
